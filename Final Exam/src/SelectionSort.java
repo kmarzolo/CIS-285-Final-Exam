b7748151c3f45aa250417669e009567847e935ef
kmarzolo <kmarzolo@umich.edu>
2021-04-27 08:29:56 -0400

    Change commit

--- Final Exam/src/SelectionSort.java
+++ Final Exam/src/SelectionSort.java
@@ -2,21 +2,18 @@ public class SelectionSort {
 	  /* Selection Sort function */
     public static void sort( int arr[] ){
         int N = arr.length;
-        int i, j, pos, temp;
+        int i, j, temp;
         for (i = 0; i < N; i++)
         {
-            pos = j;
             for (j = i+1; j < N-1; j++)
             {
-                if (arr[j] < arr[pos])
+                if (arr[i] > arr[j])
                 {
-                    pos = i;
+                  temp = arr[i];
+                  arr[i] = arr[j];
+                  arr[j]= temp;
                 }
             }
-            /* Swap arr[i] and arr[pos] */
-            temp = arr[i];
-            arr[i] = arr[pos];
-            arr[pos]= temp;
         }
-	 }
+    }
 }
