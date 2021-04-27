fe3bc7e8ff62e6bf6831ccc8a69446dfdc95ec13
kmarzolo <kmarzolo@umich.edu>
2021-04-27 08:25:27 -0400

    Before Change

--- Final Exam/src/SelectionSort.java
+++ Final Exam/src/SelectionSort.java
@@ -2,19 +2,21 @@ public class SelectionSort {
 	  /* Selection Sort function */
     public static void sort( int arr[] ){
         int N = arr.length;
-        int i, j, temp;
+        int i, j, pos, temp;
         for (i = 0; i < N; i++)
         {
+            pos = j;
             for (j = i+1; j < N-1; j++)
             {
-                if (arr[i] > arr[j])
+                if (arr[j] < arr[pos])
                 {
-                  temp = arr[i];
-                  arr[i] = arr[j];
-                  arr[j]= temp;
+                    pos = i;
                 }
             }
-
+            /* Swap arr[i] and arr[pos] */
+            temp = arr[i];
+            arr[i] = arr[pos];
+            arr[pos]= temp;
         }
 	 }
 }
